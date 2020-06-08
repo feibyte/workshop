cac483c44b22a9772894ce8691bb30d9ed583a90
fedeoo <fedeoo.zf@gmail.com>
2020-06-08 22:12:26 +1000

    Add hash table open address

---
+++ src/data-structures/hash-table/OpenAddressHashTable.js
@@ -0,0 +1,72 @@
+class OpenAddressHashTable {
+  constructor() {
+    this.bucketSize = 11;
+    this.bucket = [];
+    this.size = 0;
+  }
+
+  hash(key) {
+    let hashCode = 7; // 随机选择的初始值
+    for (let i = 0; i < key.length; i++) {
+      hashCode = ((hashCode * 31) + key.charCodeAt(i)) % this.bucketSize;
+    }
+    return hashCode;
+  }
+
+  set(key, value) {
+    let index = this.hash(key);
+    while (this.bucket[index]) {
+      index = (index + 1) % this.bucketSize;
+    }
+    this.bucket[index] = { key, value };
+    this.size++;
+    if (this.size / this.bucketSize > 0.5) {
+      this.rehashing();
+    }
+  }
+
+  rehashing() {
+    const bucket = [...this.bucket];
+    this.bucket = [];
+    this.size = 0;
+    this.bucketSize *= 2;
+    // eslint-disable-next-line no-restricted-syntax
+    for (const bucketElement of bucket) {
+      if (bucketElement) {
+        const { key, value } = bucketElement;
+        this.set(key, value);
+      }
+    }
+  }
+
+  get(key) {
+    let index = this.hash(key);
+    while (this.bucket[index]) {
+      if (this.bucket[index].key === key) {
+        return this.bucket[index].value;
+      }
+      index = (index + 1) % this.bucketSize;
+    }
+    return undefined;
+  }
+
+  delete(key) {
+    let index = this.hash(key);
+    let isCleaning = false;
+    while (this.bucket[index]) {
+      if (isCleaning) {
+        if (this.hash(this.bucket[index].key) !== index) {
+          this.set(this.bucket[index].key, this.bucket[index].value);
+          this.bucket[index] = undefined;
+        }
+      } else if (this.bucket[index].key === key) {
+        this.bucket[index] = undefined;
+        isCleaning = true;
+        this.size--;
+      }
+      index = (index + 1) % this.bucketSize;
+    }
+  }
+}
+
+export default OpenAddressHashTable;
--- src/data-structures/hash-table/__test__/OpenAddressHashTable.test.js
+++ src/data-structures/hash-table/__test__/OpenAddressHashTable.test.js
@@ -1,8 +1,8 @@
-import ChainingHashTable from '../ChainingHashTable';
+import OpenAddressHashTable from '../OpenAddressHashTable';
 
-describe('ChainingHashTable', () => {
+describe('OpenAddressHashTable', () => {
   it('should be able to get correct value after setting key and value', () => {
-    const hashTable = new ChainingHashTable();
+    const hashTable = new OpenAddressHashTable();
     hashTable.set('shark', { type: 'fish' });
     hashTable.set('tiger', { type: 'animal' });
 
@@ -11,14 +11,8 @@ describe('ChainingHashTable', () => {
     });
   });
 
-  it('return correct hash code', () => {
-    const hashTable = new ChainingHashTable();
-    expect(hashTable.hashCode('A')).toEqual(0x41);
-    expect(hashTable.hashCode('Hello')).toEqual(0x042628b2);
-  });
-
   it('should return falsy after deleting item by key', () => {
-    const hashTable = new ChainingHashTable();
+    const hashTable = new OpenAddressHashTable();
     hashTable.set('shark', { type: 'fish' });
     hashTable.set('tiger', { type: 'animal' });
 
@@ -26,14 +20,25 @@ describe('ChainingHashTable', () => {
     expect(hashTable.get('shark')).toBeFalsy();
   });
 
+
   it('should work even the key has same hash value', () => {
-    const hashTable = new ChainingHashTable();
+    const hashTable = new OpenAddressHashTable();
     hashTable.set('dog', { type: 'animal' });
     hashTable.set('bird', { type: 'bird' });
     hashTable.set('tiger', { type: 'animal' });
+    hashTable.set('shark', { type: 'fish' });
 
-    hashTable.delete('tiger');
-    expect(hashTable.get('shark')).toBeFalsy();
+    hashTable.delete('dog');
+    expect(hashTable.get('shark')).toBeTruthy();
     expect(hashTable.get('bird')).toBeTruthy();
+  });
+
+  it('should rehasing when size become larger', () => {
+    const hashTable = new OpenAddressHashTable();
+    (new Array(20)).fill(0).forEach((item, index) => {
+      hashTable.set(index, index);
+    });
+
+    expect(hashTable.size).toBe(20);
   });
 });
