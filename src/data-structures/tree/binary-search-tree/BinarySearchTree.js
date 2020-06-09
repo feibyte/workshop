b5229b298043f015839db058b908f5170e1c4cff
fedeoo <fedeoo.zf@gmail.com>
2020-06-09 23:43:02 +1000

    Add binary search tree

---
+++ src/data-structures/tree/binary-search-tree/BinarySearchTree.js
@@ -0,0 +1,148 @@
+
+// eslint-disable-next-line max-classes-per-file
+class TreeNode {
+  constructor(key) {
+    this.key = key;
+    this.parent = null;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  inorderTraverse(node, callback) {
+    if (node) {
+      this.inorderTraverse(node.left, callback);
+      callback(node);
+      this.inorderTraverse(node.right, callback);
+    }
+  }
+
+  traverse(callback) {
+    this.inorderTraverse(this.root, callback);
+  }
+
+  search(key) {
+    let node = this.root;
+    while (node && node.key !== key) {
+      if (key > node.key) {
+        node = node.right;
+      } else {
+        node = node.left;
+      }
+    }
+    return node;
+  }
+
+  getMinimum(node) {
+    while (node && node.left) {
+      node = node.left;
+    }
+    return node;
+  }
+
+  minimum() {
+    return this.getMinimum(this.root);
+  }
+
+  getMaximum(node) {
+    while (node && node.right) {
+      node = node.right;
+    }
+    return node;
+  }
+
+  maximum() {
+    return this.getMaximum(this.root);
+  }
+
+  insert(key) {
+    const newNode = new TreeNode(key);
+    let node = this.root;
+    let parent = null;
+    while (node) {
+      parent = node;
+      if (key > node.key) {
+        node = node.right;
+      } else {
+        node = node.left;
+      }
+    }
+    newNode.parent = parent;
+    if (!parent) {
+      this.root = newNode;
+      return;
+    }
+    if (key > parent.key) {
+      parent.right = newNode;
+    } else {
+      parent.left = newNode;
+    }
+  }
+
+  predecessor(node) {
+    if (node.left) {
+      return this.getMaximum(node.left);
+    }
+    let { parent } = node;
+    while (parent && parent.left === node) {
+      node = parent;
+      parent = node.parent;
+    }
+    return parent;
+  }
+
+  successor(node) {
+    if (node.right) {
+      return this.getMinimum(node.right);
+    }
+    let { parent } = node;
+    while (parent && parent.right === node) {
+      node = parent;
+      parent = node.parent;
+    }
+    return parent;
+  }
+
+  transplant(nodeU, nodeV) {
+    const { parent } = nodeU;
+    if (!parent) {
+      this.root = nodeV;
+      return;
+    }
+    if (nodeU === nodeU.parent.left) {
+      nodeU.parent.left = nodeV;
+    } else {
+      nodeU.parent.right = nodeV;
+    }
+    if (nodeV) {
+      nodeV.parent = nodeU.parent;
+    }
+  }
+
+  delete(node) {
+    if (!node.left) {
+      this.transplant(node, node.right);
+      return;
+    }
+    if (!node.right) {
+      this.transplant(node, node.left);
+      return;
+    }
+    const pivot = this.getMinimum(node.right);
+    if (pivot.parent !== node) {
+      this.transplant(pivot, pivot.right);
+      pivot.right = node.right;
+      pivot.right.parent = pivot;
+    }
+    this.transplant(node, pivot);
+    pivot.left = node.left;
+    node.left.parent = pivot;
+  }
+}
+
+export default BinarySearchTree;
---
+++ src/data-structures/tree/binary-search-tree/BinarySearchTree.test.js
@@ -0,0 +1,75 @@
+import BinarySearchTree from './BinarySearchTree';
+
+describe('BinarySearchTree', () => {
+  const makeTree = () => {
+    const tree = new BinarySearchTree();
+    tree.insert(15);
+    tree.insert(6);
+    tree.insert(18);
+    tree.insert(3);
+    tree.insert(7);
+    tree.insert(17);
+    tree.insert(20);
+    tree.insert(2);
+    tree.insert(4);
+    tree.insert(13);
+    tree.insert(9);
+    return tree;
+  };
+
+  const toArray = (tree) => {
+    const result = [];
+    tree.traverse((node) => {
+      result.push(node.key);
+    });
+    return result;
+  };
+
+  it('should return a node when calling search with given in tree', () => {
+    const tree = makeTree();
+    expect(tree.search(6).key).toBe(6);
+  });
+
+  it('should return return min node when calling minimum', () => {
+    const tree = makeTree();
+    expect(tree.minimum().key).toBe(2);
+  });
+
+  it('should return return max node when calling maximum', () => {
+    const tree = makeTree();
+    expect(tree.maximum().key).toBe(20);
+  });
+
+  it('should return previous node when calling predecessor', () => {
+    const tree = makeTree();
+    const node6 = tree.search(6);
+    expect(tree.predecessor(node6).key).toBe(4);
+
+    const node17 = tree.search(17);
+    expect(tree.predecessor(node17).key).toBe(15);
+  });
+
+  it('should return next node when calling successor', () => {
+    const tree = makeTree();
+    const node4 = tree.search(4);
+    expect(tree.successor(node4).key).toBe(6);
+
+    const node15 = tree.search(15);
+    expect(tree.successor(node15).key).toBe(17);
+  });
+
+  it('should return sorted when traverse in order', () => {
+    const tree = makeTree();
+    expect(toArray(tree)).toEqual([2, 3, 4, 6, 7, 9, 13, 15, 17, 18, 20]);
+  });
+
+  it('should return null after deleting it', () => {
+    const tree = makeTree();
+    tree.delete(tree.search(6));
+    expect(tree.search(6)).toBeFalsy();
+    tree.delete(tree.search(15));
+    tree.delete(tree.search(20));
+    tree.delete(tree.search(2));
+    expect(toArray(tree)).toEqual([3, 4, 7, 9, 13, 17, 18]);
+  });
+});
